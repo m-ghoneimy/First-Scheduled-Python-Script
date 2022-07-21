a4898c785c957672dc5debd3e506fb032050afcd
m-ghoneimy <mghoneimy1@gmail.com>
2022-07-21 22:53:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    if r.status_code == 200:
+        data = r.json()
+        temperature = data["forecast"]["temp"]
+        # print(temperature)
